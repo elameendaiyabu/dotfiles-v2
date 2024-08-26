11e6e96484a437d72dfd2c864d83985e3773dd1e
elameen <elameendaiyabu@gmail.com>
2024-08-26 23:14:30 +0000

    added configs

---
+++ nvim/lua/kickstart/plugins/null-ls.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'nvimtools/none-ls.nvim',
+    enabled = true,
+    dependencies = 'neovim/nvim-lspconfig',
+    config = function()
+      local null_ls = require 'null-ls'
+      null_ls.setup {
+        sources = {
+          null_ls.builtins.diagnostics.markdownlint,
+          null_ls.builtins.formatting.mdformat,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.formatting.goimports_reviser,
+          null_ls.builtins.formatting.gofumpt,
+          null_ls.builtins.formatting.golines,
+        },
+        root_dir = require('null-ls.utils').root_pattern('package.json', '.null-ls-root', '.neoconf.json', '.git'),
+      }
+    end,
+  },
+}
---
+++ nvim/lua/lazy-plugins.lua
@@ -0,0 +1,65 @@
+require('lazy').setup({
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/vim-tmux-navigator',
+
+  require 'kickstart/plugins/lualine',
+
+  require 'kickstart/plugins/gitsigns',
+
+  require 'kickstart/plugins/telescope',
+
+  require 'kickstart/plugins/lspconfig',
+
+  require 'kickstart/plugins/conform',
+
+  require 'kickstart/plugins/cmp',
+
+  require 'kickstart/plugins/todo-comments',
+
+  require 'kickstart/plugins/mini',
+
+  require 'kickstart/plugins/treesitter',
+
+  require 'kickstart/plugins/autotag',
+
+  require 'kickstart/plugins/null-ls',
+  require 'kickstart/plugins/noice',
+
+  --require 'kickstart/plugins/typescript-tools',
+
+  --require 'kickstart/plugins/scrollbar',
+
+  require 'kickstart/plugins/lazygit',
+
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+
+  { import = 'custom.plugins' },
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
